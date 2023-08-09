9e7940947790131eb053bf1275a1eeb496828ed5
mrkrakalakin <mrkrakalakin@gmail.com>
2023-08-09 19:34:30 -0400

    separate input file and fixed math

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// User Inputs
-const givenRate = 64; // Desired production rate per second
-const recipeName = "Iron Plate"; // Specify recipe for calculation
-const desiredRate = givenRate / 10;
+// Import user inputs
+const inputs = require('./input');
+
+// Extract desiredRate and recipeName from inputs
+const { desiredRate, recipeName } = inputs;
 
 // Import recipes
 const recipes = require('./recipes');
+
+// ... (rest of your code remains the same)
 
 // Calculate resource requirements for each recipe
 function calculateResources(recipeName, desiredRate) {
@@ -17,19 +20,19 @@ function calculateResources(recipeName, desiredRate) {
     return `Recipe not found for '${recipeName}'.`;
   }
 
-  const machineTime = recipe.machineTime; // Get the machine time from the recipe
-  const timePerOutput = recipe.outputs[recipeName] / machineTime;
+  const machineTime = recipe.machineTime;
+  const outputPerSec = recipe.outputs[recipeName] / machineTime;
 
   const requiredResources = {};
 
   // Calculate input resources
   for (const inputResource in recipe.inputs) {
     const inputRate = recipe.inputs[inputResource] * desiredRate;
-    requiredResources[inputResource] = inputRate * timePerOutput;
+    requiredResources[inputResource] = inputRate;
   }
 
   // Calculate and include number of machines needed (rounded up)
-  const machinesNeeded = Math.ceil(desiredRate * timePerOutput);
+  const machinesNeeded = Math.ceil((desiredRate / 10) / outputPerSec);
   requiredResources[recipe.machine] = machinesNeeded;
 
   return requiredResources;
@@ -40,7 +43,7 @@ if (recipes[recipeName]) {
   const resourcesRequired = calculateResources(recipeName, desiredRate);
   if (resourcesRequired) {
     const logFilePath = path.join(__dirname, 'log');
-    const logContent = `Resources required for ${givenRate} ${recipeName} per 10 seconds:\n${JSON.stringify(resourcesRequired, null, 2)}\n`;
+    const logContent = `Resources required for ${desiredRate} ${recipeName} per 10 seconds:\n${JSON.stringify(resourcesRequired, null, 2)}\n`;
     fs.writeFileSync(logFilePath, logContent);
     console.log(`Log file saved: ${logFilePath}`);
   }
