8c2d525ba9568a9636eed884e7ca3437a62182f8
mrkrakalakin <mrkrakalakin@gmail.com>
2023-08-09 17:27:00 -0400

    added copper plate recipe

--- recipes.js
+++ recipes.js
@@ -1,10 +1,16 @@
 // Define recipes
 const recipes = {
   "Iron Plate": {
-    machine: "Iron Furnace",
+    machine: "Furnace",
     machineTime: 0.8,
     inputs: { "Iron Ore": 1, "Coal": 1 / 6 },
     outputs: { "Iron Plate": 1 },
+  },
+  "Copper Plate": {
+    machine: "Furnace",
+    machineTime: 0.8,
+    inputs: { "Copper Ore": 1, "Coal": 1 / 6 },
+    outputs: { "Copper Plate": 1 },
   },
 };
 
