6015374a54df30b884261ac302344b72def61b2b
mrkrakalakin <71817741+mrkrakalakin@users.noreply.github.com>
2023-08-10 04:47:24 -0400

    testing output to log to form a tree

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ const inputs = require('./inputs');
 const recipes = require('./recipes');
 
 // Calculate resource requirements for a single recipe
-function calculateResourcesForRecipe(recipeName, desiredRate, calculatedRecipes = {}) {
-  if (calculatedRecipes[recipeName]) {
-    return calculatedRecipes[recipeName];
-  }
-
+function calculateResourcesForRecipe(recipeName, desiredRate, indent = 0) {
   const recipe = recipes[recipeName];
 
   if (!recipe) {
@@ -24,66 +20,68 @@ function calculateResourcesForRecipe(recipeName, desiredRate, calculatedRecipes 
 
   const requiredResources = {};
 
+  // Calculate and include number of machines needed (rounded up)
+  const machinesNeeded = Math.ceil(desiredRate / outputPerSec);
+  requiredResources[recipe.machine] = machinesNeeded / 10;
+
+  // Include the recipe's output as a requirement
+  requiredResources[recipeName] = desiredRate;
+
   // Recursively calculate input resources and inputs of inputs
   for (const inputResource in recipe.inputs) {
     const inputRate = recipe.inputs[inputResource] * desiredRate;
     if (recipes[inputResource]) {
-      const resourcesForInput = calculateResourcesForRecipe(inputResource, inputRate, calculatedRecipes);
-      for (const resource in resourcesForInput) {
-        requiredResources[resource] = (requiredResources[resource] || 0) + resourcesForInput[resource];
-      }
+      const resourcesForInput = calculateResourcesForRecipe(inputResource, inputRate, indent + 1);
+      requiredResources[inputResource] = resourcesForInput;
     } else {
       requiredResources[inputResource] = inputRate;
     }
   }
 
-  // Calculate and include number of machines needed (rounded up)
-  const machinesNeeded = Math.ceil( desiredRate / outputPerSec);
-  requiredResources[recipe.machine] = machinesNeeded / 10;
-
-  // Include the recipe's output as a requirement
-  requiredResources[recipeName] = (requiredResources[recipeName] || 0) + desiredRate;
-
-  calculatedRecipes[recipeName] = requiredResources;
-
   return requiredResources;
 }
 
+// Format resources with appropriate indentation
+function formatIndentedResources(resources, indent = 0, isMachines = false) {
+  let formatted = '';
 
-// Calculate total resource requirements for multiple recipes
-function calculateTotalResources(recipeInputs) {
-  const totalResources = {};
+  if (isMachines) {
+    formatted += `${'  '.repeat(indent)}Machines:\n`;
+    indent += 1;
+  }
 
-  for (const { recipeName, desiredRate } of recipeInputs) {
-    const resourcesForRecipe = calculateResourcesForRecipe(recipeName, desiredRate);
-    if (resourcesForRecipe) {
-      for (const resource in resourcesForRecipe) {
-        totalResources[resource] = (totalResources[resource] || 0) + resourcesForRecipe[resource];
+  for (const resource in resources) {
+    if (typeof resources[resource] === 'object') {
+      if (resource.endsWith(' Furnace') || resource.endsWith(' Assembler')) {
+        formatted += formatIndentedResources(resources[resource], indent, true);
+      } else {
+        formatted += `${'  '.repeat(indent)}${resource}:\n`;
+        formatted += formatIndentedResources(resources[resource], indent + 1);
       }
+    } else {
+      formatted += `${'  '.repeat(indent)}${resource}: ${resources[resource]}\n`;
     }
   }
 
-  return totalResources;
+  return formatted;
 }
+
 
 // Usage and log
-const totalResourcesRequired = calculateTotalResources(inputs);
+const logFilePath = path.join(__dirname, 'log.txt');
+let logContent = '';
 
-if (Object.keys(totalResourcesRequired).length > 0) {
-  const logFilePath = path.join(__dirname, 'log');
-  let logContent = '';
-
-  for (const { recipeName, desiredRate } of inputs) {
-    if (desiredRate !== 0) {
-      const resourcesForRecipe = calculateResourcesForRecipe(recipeName, desiredRate);
-      if (resourcesForRecipe) {
-        logContent += `Resource requirements for '${recipeName}' (${desiredRate}): \n${JSON.stringify(resourcesForRecipe, null, 2)}\n\n`;
-      }
+for (const { recipeName, desiredRate } of inputs) {
+  if (desiredRate !== 0) {
+    const resourcesForRecipe = calculateResourcesForRecipe(recipeName, desiredRate);
+    if (resourcesForRecipe) {
+      logContent += `Resource requirements for '${recipeName}' (${desiredRate}):\n`;
+      logContent += `Output: ${desiredRate}\n`;
+      logContent += formatIndentedResources(resourcesForRecipe, 1);
+      logContent += '\n';
     }
   }
-
-  fs.writeFileSync(logFilePath, logContent);
-  console.log(`Log file saved: ${logFilePath}`);
-} else {
-  console.log("No valid recipes found.");
 }
+
+fs.writeFileSync(logFilePath, logContent);
+console.log(`Log file saved: ${logFilePath}`);
--- recipes.js
+++ recipes.js
@@ -19,23 +19,64 @@ const recipes = {
     outputs: { "Aluminium Plate": 1 },
   },
   "Silicone Plate": {
-    machine: "Silicone Chemical Plant",
+    machine: "Silicone Chem Plant",
     machineTime: 1.53,
     inputs: { "Gravel": 3, "Coal": 1 },
-    outputs: { "Silicone Plate": 1 }
+    outputs: { "Silicone Plate": 1 },
   },
   "Silver Bar": {
     machine: "Silver Furnace",
     machineTime: 1.53,
     inputs: { "Silver Ore": 3 },
-
-
-  }
+    outputs: { "Silver Bar": 1 },
+  },
+  "Plastic Bar": {
+    machine: "Plastic Chem Plant",
+    machineTime: 4.16,
+    inputs: { "Coal": 1, "Oil Barrel": 1 },
+    outputs: { "Plastic Bar": 1 },
+  },
   "Iron Gear": {
     machine: "Iron Gear Assembler",
     machineTime: 1.25,
     inputs: { "Iron Plate": 1 },
     outputs: { "Iron Gear": 1 }
+  },
+  "Iron Pipe": {
+    machine: "Iron Pipe Assembler",
+    machineTime: 1.25,
+    inputs: { "Iron Plate": 1, },
+    outputs: { "Iron Pipe": 1 }
+  },
+  "Iron Frame": {
+    machine: "Iron Frame Assembler",
+    machineTime: 1.25,
+    inputs: { "Iron Plate": 1, "Iron Pipe": 1 },
+    outputs: { "Iron Frame": 1 }
+  },
+  "Motor": {
+    machine: "Motor Assembler",
+    machineTime: 1.25,
+    inputs: { "Iron Gear": 1, "Rotor": 1, "Green Wire": 1, "Iron Frame": 1 },
+    outputs: { "Motor": 1 }
+  },
+  "Titanium Plate": {
+    machine: "Titanium Furnace",
+    machineTime: 1.25,
+    inputs: { "Titanium Ore": 2 },
+    outputs: { "Titanium Plate": 1 },
+  },
+  "Turbine Fan": {
+    machine: "TFan Assembler",
+    machineTime: 1.25,
+    inputs: { "Titanium Plate": 1, "Iron Gear": 1 },
+    outputs: { "Turbine Fan": 1 }
+  },
+  "Turbine": {
+    machine: "Turbine Assembler",
+    machineTime: 1.25,
+    inputs: { "Turbine Fan": 1, "Motor": 1, "Steel Plate": 2, "Super Connector": 1 },
+    outputs: { "Turbine": 1 }
   }
 };
 
