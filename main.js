7db5665bfa228f76e162ad768c61d4a1eac86f4f
mrkrakalakin <71817741+mrkrakalakin@users.noreply.github.com>
2023-08-10 06:09:02 -0400

    separated machines

--- main.js
+++ main.js
@@ -8,37 +8,36 @@ const recipes = require('./recipes');
 const inputs = require('./inputs');
 
 // Calculate resource requirements for a single recipe
-function calculateResourcesForRecipe(recipeName, desiredRate, indent = 0) {
-  // Converts recipes to objects
+function calculateResourcesForRecipe(recipeName, desiredRate, indent = 0, machines = {}) {
   const recipe = recipes[recipeName];
-  // If the input recipe is not found, print this error
+
   if (!recipe) {
     return `Recipe not found for '${recipeName}'.`;
   }
-  // Get's machineTime from recipe
+
   const machineTime = recipe.machineTime;
-  // Convert machineTime to avg. output per second based on amount
   const outputPerSec = recipe.outputs[recipeName] / machineTime;
 
   const requiredResources = {};
 
-  // Calculate and include number of machines needed (rounded up)
-  const machinesNeeded = Math.ceil(desiredRate / outputPerSec);
-  requiredResources[recipe.machine] = machinesNeeded / 10;
+  if (recipe.machine) {
+    if (!machines[recipe.machine]) {
+      machines[recipe.machine] = 0;
+    }
+    machines[recipe.machine] += Math.ceil(desiredRate / outputPerSec) / 10;
+  }
 
-  // Include the recipe's output as a requirement
-  requiredResources[recipeName] = desiredRate;
-
-  // Recursively calculate input resources and inputs of inputs
   for (const inputResource in recipe.inputs) {
     const inputRate = recipe.inputs[inputResource] * desiredRate;
     if (recipes[inputResource]) {
-      const resourcesForInput = calculateResourcesForRecipe(inputResource, inputRate, indent + 1);
+      const resourcesForInput = calculateResourcesForRecipe(inputResource, inputRate, indent + 1, machines);
       requiredResources[inputResource] = resourcesForInput;
     } else {
       requiredResources[inputResource] = inputRate;
     }
   }
+
+  requiredResources[recipeName] = desiredRate;
 
   return requiredResources;
 }
@@ -68,18 +67,18 @@ function formatIndentedResources(resources, indent = 0, isMachines = false) {
   return formatted;
 }
 
-
 // Usage and log
 const logFilePath = path.join(__dirname, 'log.txt');
 let logContent = '';
 
 for (const { recipeName, desiredRate } of inputs) {
   if (desiredRate !== 0) {
-    const resourcesForRecipe = calculateResourcesForRecipe(recipeName, desiredRate);
+    const machines = {};
+    const resourcesForRecipe = calculateResourcesForRecipe(recipeName, desiredRate, 1, machines);
     if (resourcesForRecipe) {
       logContent += `Resource requirements for '${recipeName}' (${desiredRate}):\n`;
       logContent += `Output: ${desiredRate}\n`;
-      logContent += formatIndentedResources(resourcesForRecipe, 1);
+      logContent += formatIndentedResources({ Machines: machines, Resources: resourcesForRecipe }, 1);
       logContent += '\n';
     }
   }
