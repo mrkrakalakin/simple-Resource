3cefeb21d37968664e2a9fba6c3c76f3b7c20b0b
mrkrakalakin <71817741+mrkrakalakin@users.noreply.github.com>
2023-08-10 18:12:23 -0400

    modular update

---
+++ recipe-calculator-app/src/components/RecipeCalculator.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import calculateResourcesForRecipe from '../data/calculateRecipes';
+
+function RecipeCalculator() {
+  const [recipeName, setRecipeName] = useState('');
+  const [desiredRate, setDesiredRate] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const resourcesForRecipe = calculateResourcesForRecipe(
+      recipeName,
+      parseInt(desiredRate)
+    );
+    setResult(resourcesForRecipe);
+  };
+
+  return (
+    <div>
+      <h1>Recipe Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Recipe Name:
+          <input
+            type="text"
+            value={recipeName}
+            onChange={(e) => setRecipeName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Desired Rate:
+          <input
+            type="number"
+            value={desiredRate}
+            onChange={(e) => setDesiredRate(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Calculate</button>
+      </form>
+      <div>
+        {result && (
+          <pre>
+            {JSON.stringify(result, null, 2)}
+          </pre>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default RecipeCalculator;
