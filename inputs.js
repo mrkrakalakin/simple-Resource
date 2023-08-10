f7dce5e625b002ed598db90fe0f701827411e366
mrkrakalakin <71817741+mrkrakalakin@users.noreply.github.com>
2023-08-10 00:04:58 -0400

    inputs!

---
+++ inputs.js
@@ -0,0 +1,11 @@
+// inputs.js
+// desiredRate is every 10s
+module.exports = [
+    { recipeName: "Iron Plate", desiredRate: 100 },
+    { recipeName: "Copper Plate", desiredRate: 0 }, 
+    { recipeName: "Aluminium Plate", desiredRate: 0 },
+    { recipeName: "Silicone Plate", desiredRate: 0 }, 
+    { recipeName: "Silver Bar", desiredRate: 0 },
+    { recipeName: "Iron Gear", desiredRate: 0 }, 
+  ];
+  
